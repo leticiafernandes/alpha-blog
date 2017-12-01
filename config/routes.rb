20d3dfbb45ed683a2cf2012ff7cf399cf1f3c603
Leticia <leticia@MacBook-Pro-60.local>
2017-12-01 18:57:12 -0200

    article create and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  resources :articles
+
   get 'about', to: 'pages#about'
+
+  root 'pages#home'
 end
