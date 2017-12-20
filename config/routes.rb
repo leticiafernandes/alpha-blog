364109776cbf624353b9ab63381790fa0c0060f8
Leticia <leticia@MacBook-Pro-60.local>
2017-12-20 10:40:02 -0200

    creating unity and integration test for category

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :categories, except: [:destroy]
+
   root 'pages#home'
 end
