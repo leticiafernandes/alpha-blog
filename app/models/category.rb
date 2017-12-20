992e03582039b5068d13ca050fa116e403d94bbd
Leticia <leticia@MacBook-Pro-60.local>
2017-12-20 13:24:25 -0200

    add categories list tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+    before_save { self.name = name.downcase }
     validates :name, presence: true, length: { minimum: 3, maximum: 25 }
     validates_uniqueness_of :name
 end
